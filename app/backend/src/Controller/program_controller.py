240dfadcaccef02b3580fade88845e7ec5b28365
Joash Casiño <vanillagreen43@gmail.com>
2026-02-23 00:20:04 +0800

    added implementation for program controller and model, referencing  to the student's controller and model counterpart

--- app/backend/src/Controller/program_controller.py
+++ app/backend/src/Controller/program_controller.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from tkinter import messagebox
 
 BASE_DIR = Path(__file__).resolve().parent
 program_view_path = BASE_DIR.parent.parent.parent / 'frontend' / 'src' / 'views' / 'panels'
@@ -8,7 +9,48 @@ sys.path.insert(0, str(program_view_path))
 sys.path.insert(0, str(program_model_path))
 
 from program_model import ProgramModel
-class ProgramController:
-    def __init__(self, views):
+
+class ProgmamController:
+    def __init__(self, views, user_role):
         self.model = ProgramModel()
         self.views = views
+        self.user_role = user_role
+
+    def add_program_from_dialog(self, program_data):
+        success = self.model.add_program(program_data)
+
+        if success:
+            messagebox.showinfo("Success", "Program Added Successfully!")
+            self.views.populate_programs()
+        else:
+            messagebox.showerror("Error", "Program ID Already Exist!")
+        return success
+    
+    def update_program(self, program_data):
+        success = self.model.edit_program(program_data)
+
+        if success:
+            messagebox.showinfo("Success", "Program has been updated!")
+            self.views.populate_programs()
+        else:
+            messagebox.showerror("Error", "Please enter all required fields!")
+        return success
+
+    def delete_program(self, program_code):
+        if self.model.program_has_students(program_code):
+            messagebox.showwarning(
+                "Cannot Delete",
+                f"Program '{program_code}' currently has enrolled students\n"
+                "Delete or reasign the students first."
+            )
+            return
+
+        success = self.model.delete_program(program_code)
+        if success:
+            messagebox.showinfo("Success", "Program deleted successfully")
+            self.views.populate_programs()
+        else:
+            messagebox.showerror("Error", "program not found.")
+
+    
+        
--- app/backend/src/Model/program_model.py
+++ app/backend/src/Model/program_model.py
@@ -1,3 +1,102 @@
+import csv
+from os import read
+from pathlib import Path
+
+program_csv = Path(__file__).resolve().parent.parent.parent / 'data' / 'programs.csv'
 class ProgramModel:
     def __init__(self):
-        pass
+        self.csv_file = program_csv
+        self.headers = ['Program Code', 'Program Name', 'College Code', 'College Name']
+
+    def add_program(self, program_data):
+        try:
+            if self.program_exist(program_data.get('Program Code')):
+                return False
+            
+            with open(self.csv_file, 'a', newline='', encoding='utf-8') as file:
+                writer = csv.DictWriter(file, fieldnames=self.headers)
+                writer.writerow(program_data)
+
+                return True
+            
+        except Exception as e:
+            print(f"Error adding programs: {e}")
+            return False
+        
+    def program_exist(self, program_code):
+        try:
+            with open(self.csv_file, 'r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    if row['Program Code'] == program_code:
+                        return True
+            return False
+        except FileNotFoundError:
+            return False
+        
+    def edit_program(self, program_data):
+        try:
+            rows = []
+            found = False
+
+            with open(self.csv_file, 'r', newline='', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    if row["Program Code"] == program_data.get("Program Code"):
+                        rows.append(program_data)
+                        found = True
+                    else:
+                        rows.append(row)
+
+            if not found:
+                return False
+            
+            with open(self.csv_file, 'w', newline='', encoding='utf-8') as file:
+                writer = csv.DictWriter(file, fieldnames=self.headers)
+                writer.writeheader()
+                writer.writerows(rows)
+
+            return True
+
+        except Exception as e:
+            print(f"Error update program: {e}")
+            return False
+
+    def delete_program(self, program_id):
+        try:
+            rows = []
+            found = False
+
+            with open(self.csv_file, 'r', newline='', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    if row['Program Code'] == program_id:
+                        found = True
+                    else:
+                        rows.append(row)
+            
+            if not found:
+                return False
+
+            with open(self.csv_file, 'w', newline='', encoding="utf-8") as file:
+                writer = csv.DictWriter(file, fieldnames=self.headers)
+                writer.writeheader()
+                writer.writerows(rows)
+
+            return True
+    
+        except Exception as e:
+            print(f"Error deleting program {e}")
+            return False
+    
+    def program_has_students(self, program_code):
+        try:
+            student_csv = self.csv_file.parent / 'students.csv'
+            with open(student_csv, 'r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    if row["Program"] == program_code:
+                        return True    
+            return False
+        except FileNotFoundError:
+            return False
